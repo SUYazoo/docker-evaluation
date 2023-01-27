8c20cb71456760130acfda4619a0a7aba2ed3863
Yatté <yatte@transparence.pro>
2023-01-27 16:03:04 +0100

    Commit Exercice N°2

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 docker network create network
+docker run --network=network -e PORT=8080 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker1 -e ADD=true -e MULT=false --name worker -d dockworker
+docker run --network=network -e PORT=8081 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker1 -e ADD=false -e MULT=true --name worker1 -d dockworker
+docker run --network=network -e PORT=8082 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker2 -e ADD=true -e MULT=false --name worker2 -d dockworker
 docker run --network=network -e PORT=3000 --name planner -d dockplanner
-docker run --network=network -e PORT=8080 -e PLANNER=http://planner:3000 -e ADDRESS=http://worker --name worker -d dockworkers
